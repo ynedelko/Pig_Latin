aa781c876989d681706e5b8029e22f6d27ae03e4
Yelena Nedelko & George Denkey <student@epicodus.com>
2015-09-09 16:42:13 -0700

    treat the 'y' as a vowel, for words that don't start with a 'y'.

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,30 @@ var pigLatin = function(word) {
   var vowel_not_found = true;
   var new_end = "";
 
-
-  while (vowel_not_found) {
-    if(isVowel(word[0]))  {
-      vowel_not_found = false;
-  } else if ((word[0] === "q") && (word[1] == "u")) {
-    new_end = new_end + word[0] + word[1];
-     word = word.slice(2);
-  } else {
+// we took it out of the while look because we're only dealing with the first letter, the while loop will loop through all the letters
+  if (word[0] === "y") {
     new_end = new_end + word[0];
     word = word.slice(1);
+    return word + new_end + "ay";
+  }
+  while (vowel_not_found) {
+    if (isVowel(word[0])) {
+      vowel_not_found = false;
+    } else if ((word[0] === "q") && (word[1] === "u")) {
+      new_end = new_end + word[0] + word[1];
+      word = word.slice(2);
+    } else {
+      new_end = new_end + word[0];
+      word = word.slice(1);
     }
   }
   return word + new_end + "ay";
 };
 
+// !we want to stop the loop once y is the first letter!
+
 var isVowel = function(letter) {
-  var vowels = ["a", "e", "i", "o", "u"];
+  var vowels = ["a", "e", "i", "o", "u", "y"];
   if (vowels.indexOf(letter) != -1)  {
     return true;
   } else {
